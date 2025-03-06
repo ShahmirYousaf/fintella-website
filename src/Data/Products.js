70c75f6aa298ce56745b7f05874cf1b211584e31
Shahmir Yousaf <110347102+ShahmirYousaf@users.noreply.github.com>
2025-03-06 17:14:22 +0500

    Little Fixes

--- src/Data/Products.js
+++ src/Data/Products.js
@@ -4,14 +4,21 @@ export const products = [
       title: "Chat GPT with Microsoft",
       price: 45000,
       description: "ChatGPT Excel for Beginners - Learn Microsoft Excel Fast With the Help of Artificial Intelligence: Your step by step guide with tips, tricks and the all-important prompts! Discover the power of Excel 2023 with the help of artificial intelligence! This comprehensive guide is sure to become one of your most useful and regularly referenced chat gpt books, designed specifically for beginners. Learn how to harness the capabilities of ChatGPT-3 / ChatGPT-4 to master Excel quickly and efficiently.This ChatGPT book is a must-have for anyone looking to build proficiency in Excel, whether you're a complete beginner or simply looking to refresh your knowledge. Don’t miss out on this amazing resource, complete with tips, tricks, and the all-important prompts!",
-      image: '/images/chatgpt_with_msexcel.jpeg'  // Use the path relative to the public directory
+      image: '/images/chatgpt_with_msexcel.jpeg' 
     },
     {
       id: 2,
       title: "Microsoft 365 Excel and Excel 2021 pivot tables",
       price: 50000,
       description: "Use Microsoft 365 Excel and Excel 2021 pivot tables and pivot charts to produce powerful, dynamic reports in minutes: take control of your data and your business! Even if you've never created a pivot table before, this book will help you leverage all their flexibility and analytical power― including important recent improvements in Microsoft 365 Excel. Drawing on more than 30 years of cutting-edge Excel experience, MVP Bill Jelen (“MrExcel”) shares practical “recipes” for solving real business problems, expert insights for avoiding mistakes, and advanced tips and tricks you'll find nowhere else.",
-      image: '/images/ms_excel_pivot.jpeg'  // Use the path relative to the public directory
-    }
+      image: '/images/ms_excel_pivot.jpeg'  
+    },
+    {
+        id: 3,
+        title: "Collect, Combine, and Transform Data Using Power Query in Power BI and Excel (Business Skills) 2nd Edition",
+        price: 25000,
+        description: "Transform your data analysis experience with Power Query, the ultimate tool for importing, reshaping, and cleansing data through a user-friendly interface. Whether you’re using Power BI, Excel, or other Microsoft products, Power Query’s capabilities are at your fingertips. Renowned Power Query experts Daniil Maslyuk and Gil Raviv guide you through mastering this indispensable tool, helping you eliminate tedious manual data preparation, tackle common issues, and avoid potential pitfalls. In this updated edition, you'll delve into comprehensive analytics challenges, seamlessly integrating your skills into a realistic, final project. By the end, you'll possess the expertise to handle any data and convert it into actionable insights.",
+        image: '/images/powerBi.jpeg'  
+      }
   ];
   
--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { products } from '../../Data/Products';
+import { FaTruck, FaExchangeAlt, FaCheckCircle } from 'react-icons/fa';
 import './ProductDetails.css';
 
 const ProductDetail = () => {
   const { id } = useParams();
-  console.log("Hello")
   const product = products.find((product) => product.id === parseInt(id));
+
+  const [quantity, setQuantity] = useState(1);
+
+  const increaseQuantity = () => setQuantity(quantity + 1);
+  const decreaseQuantity = () => setQuantity(quantity > 1 ? quantity - 1 : 1);
 
   if (!product) {
     return <div>Product not found!</div>;
@@ -20,10 +25,31 @@ const ProductDetail = () => {
         </div>
         <div className="product-info-Det">
           <h1 className="product-title-Det">{product.title}</h1>
-          <p className="product-price-Det">${product.price.toFixed(2)}</p>
-          <p className="product-descriptionv">{product.description}</p>
+          <p className="product-price-Det">Rs{product.price.toFixed(2)}</p>
+          <p className="product-description-Det">{product.description}</p>
+          
           <div className="product-actions-Det">
+            <div className="quantity-selector">
+              <button className="quantity-btn" onClick={decreaseQuantity}>-</button>
+              <span className="quantity-display">{quantity}</span>
+              <button className="quantity-btn" onClick={increaseQuantity}>+</button>
+            </div>
             <button className="add-to-cart-btn">Add to Cart</button>
+          </div>
+
+          <div className="product-policies-Det">
+            <div className="policy-item">
+              <FaExchangeAlt className="policy-icon" />
+              <span>No Return Policy</span>
+            </div>
+            <div className="policy-item">
+              <FaTruck className="policy-icon" />
+              <span>Fully Insured</span>
+            </div>
+            <div className="policy-item">
+              <FaCheckCircle className="policy-icon" />
+              <span>All taxes and customer duties included</span>
+            </div>
           </div>
         </div>
       </div>

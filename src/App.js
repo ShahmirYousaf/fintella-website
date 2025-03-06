b247ec5347e4f489e90d9c7c056f32f20c8c0d32
Wania <waniatariq18@gmail.com>
2025-03-06 22:58:38 +0500

    Adding Home Page

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Products from './Pages/Product/Product';
 import ProductDetails from './Pages/ProductDetails/ProductDetails';
+import Home from './Pages/Home/Home';
 import FAQ from './Pages/FAQ/FAQ';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
-      <Route path="/" element={<Products />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/faq" element={<FAQ />} />
-        <Route path="/products/:id" element={<ProductDetails/>} />
+        <Route path="/products/:id" element={<ProductDetails />} />
       </Routes>
-      </Router>
+    </Router>
   );
 }
 
